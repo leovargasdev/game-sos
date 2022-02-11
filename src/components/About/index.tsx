460dee6999c840c50395fe06392722f723e364d1
Leonardo Vargas <leu1607@hotmail.com>
2022-02-10 21:07:06 -0300

    feat: adicionando a seção aboutGame

---
+++ src/components/About/index.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import styles from './styles.module.scss'
+
+export const About = () => {
+  const [images, setImages] = useState<string[]>([
+    'screenshot-1.png',
+    'screenshot-2.png',
+    'screenshot-3.png',
+    'screenshot-4.png',
+    'screenshot-5.png'
+  ])
+  const [activeImage, setActiveImage] = useState<string>('screenshot-1.png')
+
+  const handleChangeActiveImage = () => {
+    const firstImage = images.shift()
+    setImages([...images, firstImage])
+    setActiveImage(images[0])
+  }
+
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.description}>
+          <h3>What is SOS?</h3>
+          <h1>social battle royale game</h1>
+          <p>
+            Each round, you and 15 other contestants compete to escape a deadly
+            island filled with monsters. The trick is: three people can survive.
+            Will you run solo or form friendships with others to escape?
+          </p>
+          <p>
+            Making the right decisions could be the difference between{' '}
+            <span>life and death.</span>
+          </p>
+        </div>
+
+        <div className={styles.carousel}>
+          <ul>
+            {images.map(image => (
+              <li key={image} className={styles.image}>
+                <img src={image} alt="" />
+              </li>
+            ))}
+          </ul>
+
+          <div className={styles['carousel-controll']}>
+            <span className={activeImage.includes('1') ? styles.active : ''} />
+            <span className={activeImage.includes('2') ? styles.active : ''} />
+            <span className={activeImage.includes('3') ? styles.active : ''} />
+            <span className={activeImage.includes('4') ? styles.active : ''} />
+            <span className={activeImage.includes('5') ? styles.active : ''} />
+          </div>
+
+          <button onClick={handleChangeActiveImage}>
+            <img src="/icons/switch.svg" alt="" />
+          </button>
+        </div>
+      </div>
+    </section>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 
 import { Header } from 'components/Header'
 import { Banner } from 'components/Banner'
+import { About } from 'components/About'
 
 import styles from 'styles/home.module.scss'
 
@@ -15,6 +16,8 @@ export default function Home() {
         <Header />
         <Banner />
       </div>
+
+      <About />
     </div>
   )
 }
