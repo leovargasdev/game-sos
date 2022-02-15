28fc1fb2b52c094cbc3809ec3639e2e7028987ae
Leonardo Vargas <leu1607@hotmail.com>
2022-02-14 22:18:33 -0300

    feat: metodo para selecionar um idioma

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,18 @@ import { FaChevronDown } from 'react-icons/fa'
 
 import styles from './styles.module.scss'
 
+type Language = 'eng' | 'rus' | 'fra' | 'deu' | 'pt-br'
+
+const LANGUAGES: Language[] = ['eng', 'rus', 'fra', 'deu', 'pt-br']
+
 export const Header = () => {
-  const [activeLanguages, setActiveLanguages] = useState<boolean>(false)
+  const [activeLanguage, setActiveLanguage] = useState<Language>('eng')
+  const [toggleLanguages, setToggleLanguages] = useState<boolean>(false)
+
+  const handleChangeLanguage = (language: Language) => {
+    setActiveLanguage(language)
+    setToggleLanguages(false)
+  }
 
   return (
     <header className={styles.container}>
@@ -34,15 +44,23 @@ export const Header = () => {
         <div className={styles.languages}>
           <button
             type="button"
-            onClick={() => setActiveLanguages(!activeLanguages)}
+            onClick={() => setToggleLanguages(!toggleLanguages)}
           >
-            ENG <FaChevronDown />
+            {activeLanguage} <FaChevronDown />
           </button>
 
-          <ul className={activeLanguages ? styles.active : ''}>
-            <li>RUS</li>
-            <li>FRA</li>
-            <li>DEU</li>
+          <ul className={toggleLanguages ? styles.active : ''}>
+            {LANGUAGES.map(language => (
+              <li
+                key={language}
+                onClick={() => handleChangeLanguage(language)}
+                className={
+                  language === activeLanguage ? styles['selected-language'] : ''
+                }
+              >
+                {language}
+              </li>
+            ))}
           </ul>
         </div>
 
